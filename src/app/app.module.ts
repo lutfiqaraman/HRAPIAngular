cd993a69f6cd7a196978b9e274eac81661fb97d4
lutfiqaraman <lutfiqaraman@gmail.com>
2022-12-26 19:06:55 +0300

    create department component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent
   ],
   imports: [
     BrowserModule,
