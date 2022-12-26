c360f72e3874c59a7c33bafc18bfff82411d27c1
lutfiqaraman <lutfiqaraman@gmail.com>
2022-12-26 19:09:59 +0300

    create department component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentComponent } from './department/department.component';
+import { AddEditDepartmentComponent } from './department/add-edit-department/add-edit-department.component';
+import { ShowDepartmentComponent } from './department/show-department/show-department.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartmentComponent
+    DepartmentComponent,
+    AddEditDepartmentComponent,
+    ShowDepartmentComponent
   ],
   imports: [
     BrowserModule,
