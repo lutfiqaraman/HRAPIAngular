557065bb208c6a267798d20d20129fac55cadae0
lutfiqaraman <lutfiqaraman@gmail.com>
2022-12-28 15:06:29 +0300

    build the employee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { DepartmentComponent } from './department/department.component';
 import { AddEditDepartmentComponent } from './department/add-edit-department/add-edit-department.component';
 import { ShowDepartmentComponent } from './department/show-department/show-department.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { AddEditEmployeeComponent } from './employee/add-edit-employee/add-edit-employee.component';
+import { ShowEmployeeComponent } from './employee/show-employee/show-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DepartmentComponent,
     AddEditDepartmentComponent,
-    ShowDepartmentComponent
+    ShowDepartmentComponent,
+    EmployeeComponent,
+    AddEditEmployeeComponent,
+    ShowEmployeeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/employee/show-employee/show-employee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-employee',
+  templateUrl: './show-employee.component.html',
+  styleUrls: ['./show-employee.component.css']
+})
+export class ShowEmployeeComponent {
+
+}
