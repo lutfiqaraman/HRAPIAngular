35c8228caec8b0e32c777105556ae39e636842b3
lutfiqaraman <lutfiqaraman@gmail.com>
2022-12-28 15:08:28 +0300

    build the employee route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DepartmentComponent} from "./department/department.component";
+import {EmployeeComponent} from "./employee/employee.component";
 
 const routes: Routes = [
+  { path: 'employee', component: EmployeeComponent},
   { path: 'department', component: DepartmentComponent }
 ];
 
