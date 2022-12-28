68401510ceb5128e9cb702074bdf3e5ba5d763db
lutfiqaraman <lutfiqaraman@gmail.com>
2022-12-28 15:03:39 +0300

    build the route of department

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DepartmentComponent} from "./department/department.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'department', component: DepartmentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
