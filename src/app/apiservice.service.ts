f5516df48c0890e1c615771d7cbeee329888513f
lutfiqaraman <lutfiqaraman@gmail.com>
2022-12-29 21:11:05 +0300

    add api url

--- src/app/apiservice.service.ts
+++ src/app/apiservice.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiserviceService {
 
-  constructor() { }
+  readonly apiURL = 'https://localhost:7266/api/';
+
+  constructor(private http: HttpClient) { }
 }
